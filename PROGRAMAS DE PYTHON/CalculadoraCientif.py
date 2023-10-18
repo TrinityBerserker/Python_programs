9ba2bf82883480fb7fda1eaf1c60f1a06c8abe6b
PaganoBerserker <47154857+PaganoBerserker@users.noreply.github.com>
2023-10-18 02:38:39 -0600

    Uploading more programs in python.

---
+++ PROGRAMAS DE PYTHON/CalculadoraCientif.py
@@ -0,0 +1,39 @@
+import math
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def potencia(a, b):
+    return a ** b
+
+def raiz_cuadrada(a):
+    return math.sqrt(a)
+
+def seno(a):
+    return math.sin(a)
+
+def coseno(a):
+    return math.cos(a)
+
+def tangente(a):
+    return math.tan(a)
+
+# Ejemplo de uso
+print(suma(5, 3))  # Output: 8
+print(resta(10, 4))  # Output: 6
+print(multiplicacion(2, 6))  # Output: 12
+print(division(15, 3))  # Output: 5
+print(potencia(2, 3))  # Output: 8
+print(raiz_cuadrada(16))  # Output: 4
+print(seno(math.pi/2))  # Output: 1.0
+print(coseno(0))  # Output: 1.0
+print(tangente(math.pi/4))  # Output: 1.0
